aec4c267fd3be9a1f2ce3721a2900a269bcf1735
kanachrisi <kanachrisi@yahoo.fr>
2022-09-02 09:24:44 -0400

    End of Section 8

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using API.Entities;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        async Task<AppUser> IUserRepository.GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        async Task<AppUser> IUserRepository.GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(u => u.UserName == username);
+        }
+
+        async Task<IEnumerable<AppUser>> IUserRepository.GetUsersAsync()
+        {
+            return await _context.Users.Include(p => p.Photos).ToListAsync();
+        }
+
+        async Task<bool> IUserRepository.SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        void IUserRepository.Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
