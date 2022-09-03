7a7a20d657824f8bb04a33497466a62910f24c91
kanachrisi <kanachrisi@yahoo.fr>
2022-09-02 21:24:07 -0400

    End of section 9

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Member} from 'src/app/models/member';
+import {MembersService} from 'src/app/services/members.service';
+import {NgxGalleryOptions} from '@kolkov/ngx-gallery';
+import {NgxGalleryImage} from '@kolkov/ngx-gallery';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
 
 @Component({
-  selector: 'app-member-detail',
+  selector: 'member-detail',
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
-export class MemberDetailComponent implements OnInit {
+export class MemberDetailComponent implements OnInit 
+{
+  member: Member;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
-  constructor() { }
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    this.loadMember();
+    
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ]
+    
+    
+  }
+  
+  getImages(): NgxGalleryImage[]
+  {
+    const imagesUrls = [];
+    for (const photo of this.member.photos)
+    {
+      imagesUrls.push({
+        small: photo?.url,
+        medium: photo?.url,
+        big: photo?.url
+      })
+    }
+    return imagesUrls;
+  }
+  
+  loadMember()
+  {
+    let username = this.route.snapshot.paramMap.get('username');
+    this.memberService.getMember(username).subscribe(member =>
+    {
+      this.member = member;
+      this.galleryImages = this.getImages();
+    })
   }
 
 }
